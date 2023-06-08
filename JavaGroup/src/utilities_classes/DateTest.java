f02649d3dda43de102c2dcf3383ecd6adccee1e1
unknown <princechaudhary0001001@gmail.com>
2023-06-08 14:14:42 +0545

    June 6 2023 2:14 PM

--- JavaGroup/src/utilities_classes/DateTest.java
+++ JavaGroup/src/utilities_classes/DateTest.java
@@ -1,14 +1,19 @@
 package utilities_classes;
+
 import java.util.Date;
+import java.sql.Timestamp;
 
 public class DateTest {
 
 	public static void main(String[] args) {
-		//long	getTime()
+		// long getTime()
 		Date date = new Date();
 		long d = date.getTime();
-		System.out.println(d); //TimeStamp
-		
-		//How to convert Time-stamp to date/time?
+		System.out.println(d); // TimeStamp
+
+		// How to convert Time-stamp to date/time?
+		Timestamp timestamp = new Timestamp(d);
+		Date convertedDate = new Date(timestamp.getTime());
+		System.out.println(convertedDate);// timestamp to date
 	}
 }
--- JavaGroup/src/utilities_classes/LocalDateTest.java
+++ JavaGroup/src/utilities_classes/LocalDateTest.java
@@ -1,14 +1,47 @@
 package utilities_classes;
+
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Date;
 
 public class LocalDateTest {
 
+	
+
 	public static void main(String[] args) {
-		//DayOfWeek	getDayOfWeek()
-		//LocalDate ld = new Date();
+		// DayOfWeek getDayOfWeek()
+		// LocalDate ld = new Date();
+
+		// System.out.println(localdate);
+
+		// Month getMonth()
+		/*
+		 * LocalDate localdate = LocalDate.now(); Month localdate1
+		 * =localdate.getMonth(); System.out.println(localdate1);
+		 */
+
+		// DayOfWeek getDayOfWeek()
+		/*LocalDate localdate = LocalDate.now();
+		DayOfWeek localdate2=localdate.getDayOfWeek();
+		System.out.println(localdate2);
+		*/
 		
-
+		//LocalDateTime atStartOfDay()
+		/*LocalDate localdate = LocalDate.now();
+		LocalDateTime localdate3=localdate.atStartOfDay();
+		System.out.println(localdate3);
+		*/
+		
+		//LocalDateTime atTime(int hour, int minute)
+		/*int hour=8;
+		int minute=38;
+		LocalDate localdate = LocalDate.now();
+		LocalDateTime localdate4=localdate.atTime( hour,  minute);
+		System.out.println(localdate4);	
+		*/
 	}
-
 }
+
+

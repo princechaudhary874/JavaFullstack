2b7d072de80027f1ac359f9d023fcb65ee6f196a
unknown <princechaudhary0001001@gmail.com>
2023-08-02 23:17:37 +0545

    August 02 2023

---
+++ DSAQuestion/src/linearSearch/SearchIn2D.java
@@ -0,0 +1,8 @@
+package linearSearch;
+
+public class SearchIn2D {
+	public static void main(String[] args) {
+		int[][] arr = { { 32, 3432, 55, 22 }, { 1, 2, 3 }, { 4, 57, 5 }, { 9, 7, 8, 0 } };
+	}
+	static int search
+}
